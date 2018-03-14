4c5e37633e69d00d4ec3edcc473cbee828ad33b0
Ujjwal Gupta <ujjwalgupta1262@gmail.com>
2018-03-15 00:56:53 +0530

    "Code for viterbi algorithm in python"

--- viterbi.py
+++ viterbi.py
@@ -26,7 +26,8 @@ class state:
 	def __init__(self,iden):
 		self.iden = iden
 		self.obsProbs = map(float,list(np.random.rand(num_observs)))
-		self.score = 0
+		self.prev_score = 0
+		self.next_score = 0
 		self.state_seq = []
 
 #get list of states
@@ -35,26 +36,30 @@ state_list = [state(str(i)) for i in range(num_states)]
 #function to initialize states 
 def init_state(state):
 	index = int(state.iden)
-	state.score = initial_prob[index]*state.obsProbs[obs_seq[0]]
+	state.prev_score = initial_prob[index]*state.obsProbs[obs_seq[0]]
+	state.next_score = state.prev_score
 	state.state_seq = [state]
 
 #initialize all the states
 for i in state_list:
 	init_state(i)
 
+#returns index of maximum element
 def maxIndex(l):
 	return l.index(max(l))
 
 #the main viterbi algorithm
 for i in range(time_steps):
 	for j in range(num_states):
-		m = [(state_list[k].score*transition_prob[k][j]) for k in range(num_states)]
+		m = [(state_list[k].prev_score*transition_prob[k][j]) for k in range(num_states)]
 		maximum = max(m)
 		index = maxIndex(m)
-		state_list[j].score = maximum*(state_list[j].obsProbs[obs_seq[i]])
+		state_list[j].next_score = maximum*(state_list[j].obsProbs[obs_seq[i]])
 		state_list[j].state_seq = [state_list[index]] + state_list[j].state_seq
+	for i in state_list:
+		i.prev_score = i.next_score
 
-temp = [state_list[i].score for i in range(num_states)]
+temp = [state_list[i].next_score for i in range(num_states)]
 #find max score
 max_prob = max(temp)
 
@@ -65,7 +70,7 @@ optimal_seq = [i.iden for i in l]
 
 print("Max_prob: " + str(max_prob) + "\n")
 print("Optimal sequence of states: " + str(optimal_seq) + "\n")
-print("Given observation sequence: " + str(obs_seq) + "\n")
+print("Observation sequence: " + str(obs_seq) + "\n")
 print("Initial probabilities: " + str(initial_prob) + "\n") 
-print("Transition probabilities: " + str(transition_prob) + "\n")
+
 
